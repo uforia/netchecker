0af60bc3ec24651304b03e26fd70f7f63ba1dbdf
Arnim Eijkhoudt <uforia@dhcp.net>
2017-04-14 15:13:18 +0200

    - Added: autodownload/-update feature for the MaxMind GeoLite db's - Added: quiet mode operation, outputs results in CSV format - Added: progress indicator - Added: specifying the AS numbers/names on the command-line (no editing required) - Updated: README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Description  
 
-(c) Arnim Eijkhoudt <arnime _squiggly_ kpn-cert.nl>, 2017, KPN-CERT, GPLv2 license
+(c) Arnim Eijkhoudt \<arnime _squiggly_ kpn-cert.nl\>, 2017, KPN-CERT, GPLv2 license
   
 Netchecker lets you offline-check a list of IP addresses against known AS numbers/names. Works with both IPv4 and IPv6! This tool is useful
 for checking of e.g. leaked/dumped IPs against ASNs to see if there are matches, without having to use internet sources and thereby risking
@@ -15,18 +15,15 @@ the possibility of announcing your interest in the checked IPs.
 # Installation  
   
 1) git clone https://github.com/uforia/netchecker.git
-2) pip install netaddr / apt-get install python-netaddr / emerge dev-python/netaddr / or whatever you use for package management
-3) download the GeoLite ASN databases: http://dev.maxmind.com/geoip/legacy/geolite/ and unpack them in the netchecker directory
+2) pip install netaddr, apt-get install python-netaddr, emerge dev-python/netaddr or whatever else you use for package management
+3) download the GeoLite ASN databases by running ./netchecker.py -u
 
-# Configuration and usage  
+# Usage  
   
 1) Create a list of IPs in plaintext, one IP per line, in a single file, e.g.: 'ips.txt'
-2) Open netchecker.py in an editor and change the 'MyASN' variable's contents at the top to reflect your AS names/numbers
-3) Run netchecker: ./netchecker.py ips.txt
+2) Run netchecker: ./netchecker.py and check the command line options
 
 # Caveats, miscellaneous, TODO, etc.  
   
 - The more AS names/numbers you check, the longer it takes to build the list of netblocks to verify against!
-- This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com
-- Configuration option parsing at the CLI to specify the MyASNs
-- An automatic GeoIPLite downloader might be included in the future
+- This product uses GeoLite data created by MaxMind, available from http://www.maxmind.com
--- netchecker.py
+++ netchecker.py
@@ -1,85 +1,163 @@
 #!/usr/bin/env python
 
-### My ASNs is a Python tuple of AS numbers and/or AS names, as listed in the MaxMind GeoIP
-### files. Pattern matching is based on regex matching: 'KPN' will also yield the 'KPNQwest'
-### netblocks. The easiest way of preventing false matches is by adding a space or something
-### similar at the end of the list item. Remember to add a loose 'comma' to a single entry
-### in the tuple, to prevent it being split into individual characters.
-MyASNs=('KPN B.V.',)
-
-### More examples:
-#MyASNs=('Level3','Qwest')
-#MyASNs=('AS286 ','AS1136 ','AS2043 ','AS5615 ','AS8737 ','AS12871','AS21286')
+### (c) 2017 Arnim Eijkhoudt <arnime _squigglything_ kpn-cert.nl>, GPLv2 licensed except where
+### otherwise indicated (e.g.: netaddr / MaxMind stuff).
 
 ### This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com.
-### You might have to change the following if MaxMind changes the filename scheme.
-### Names of GeoIP CSV files, grab the ASN ZIP archives from the MaxMind website.
+### You might have to change the following if MaxMind changes the filename URLs and/or schemes.
+### URLs and names of GeoIP CSV files, used for grabbing the ZIP archives from the MaxMind website.
+GeoIPURL='http://download.maxmind.com/download/geoip/database/asnum/'
+GeoIPURLzip='GeoIPASNum2.zip'
+GeoIPv6URLzip='GeoIPASNum2v6.zip'
 GeoIP='GeoIPASNum2.csv'
 GeoIPv6='GeoIPASNum2v6.csv'
 
 ### The 'netaddr' module can be downloaded through PyPi (pip install ...) or installed
 ### through your package manager of choice.
-import netaddr,sys,re,csv
+import netaddr,sys,re,csv,optparse,urllib2,zipfile,os
+
+def UpdateGeoIP():
+	"""
+	Download the GeoLite IP databases from MaxMind and unpack them into the current working directory.
+	This will overwrite any existing file(s) with the same name.
+	"""
+	try:
+		response=urllib2.urlopen(GeoIPURL+GeoIPURLzip)
+	except urllib2.URLError as e:
+		print("An error occurred downloading "+GeoIPURL+GeoIPURLzip+": "+e.reason)
+	try:
+		with open(GeoIPURLzip,'wb') as f:
+			f.write(response.read())
+	except IOError:
+		print("An error occurred writing "+GeoIPURLzip+ " to disk!")
+	try:
+		with zipfile.ZipFile(GeoIPURLzip,'r') as z:
+			with open(GeoIP,'wb') as f:
+				f.write(z.read(GeoIP))
+				os.unlink(GeoIPURLzip)
+	except:
+		print("An error occured unzipping "+GeoIPURLzip)
+	try:
+		response=urllib2.urlopen(GeoIPURL+GeoIPv6URLzip)
+	except urllib2.URLError as e:
+		print("An error occurred downloading "+GeoIPURL+GeoIPv6URLzip+": "+e.reason)
+	try:
+		with open(GeoIPv6URLzip,'wb') as f:
+			f.write(response.read())
+	except IOError:
+		print("An error occurred writing "+GeoIPv6URLzip+ " to disk!")
+	try:
+		with zipfile.ZipFile(GeoIPv6URLzip,'r') as z:
+			with open(GeoIPv6,'wb') as f:
+				f.write(z.read(GeoIPv6))
+				os.unlink(GeoIPv6URLzip)
+	except:
+		print("An error occured unzipping "+GeoIPv6URLzip)
 
 def IntIPtoStr(integervalue):
+	"""
+	Convert an integer-formatted IPv4 address into a tuple which can be converted into dot notation
+	"""
 	return str(int(integervalue/16777216)%256),str(int(integervalue/65536)%256),str(int(integervalue/256)%256),str(int(integervalue)%256)
 
-def BuildNetblocks(MyASNs):
+def BuildNetblocks(ASNs,options):
+	"""
+	Build a list of netblocks based on the user-given list of AS numbers/names
+	"""
 	try:
 		if GeoIP:
 			with open(GeoIP,'rb') as f:
-				IPv4MyASNs=tuple(csv.reader(f))
+				IPv4ASNs=tuple(csv.reader(f))
 		if GeoIPv6:
 			with open(GeoIPv6,'rb') as f:
-				IPv6MyASNs=tuple(csv.reader(f))
+				IPv6ASNs=tuple(csv.reader(f))
 	except IOError:
-		print("E) Error opening/reading ASN file(s): "+IPv4MyASNs+" or "+IPv6MyASNs+"!")
+		print("E) Error opening/reading ASN file(s): "+IPv4ASNs+" or "+IPv6ASNs+"!")
 		sys.exit(1)
-	netblocks=[]
-	print("1) Building the list of netblocks (this may take a while, depending on the number of ASNs) ...")
-	for ASN in MyASNs:
-		print('2) Searching for AS number/name: '+ASN)
-		prog=re.compile(ASN)
-		for IPv4ASN in IPv4MyASNs:
+	netblocks={}
+	if options.verbose:
+		print("1) Building the list of netblocks (this may take a while, depending on the number of ASNs) ...")
+	for ASN in ASNs:
+		if options.verbose:
+			print('2) Searching for AS number/name: '+ASN)
+		prog=re.compile(ASN,re.IGNORECASE)
+		if options.verbose:
+			ipv4count=0
+		for IPv4ASN in IPv4ASNs:
 			try:
 				netblockstartint,netblockendint,ASname=IPv4ASN
 			except:
-				print("E) An error occurred persing the IPv4ASN: "+IPv4ASN)
-			if prog.search(ASname):
+				print("E) An error occurred parsing the IPv4ASN: "+IPv4ASN)
+			if prog.search(ASname,re.IGNORECASE):
 				netblockstart='.'.join(IntIPtoStr(int(netblockstartint)))
 				netblockend='.'.join(IntIPtoStr(int(netblockendint)))
-				netblock=netaddr.cidr_merge(list(netaddr.iter_iprange(netblockstart,netblockend)))
-				netblocks.append(netblock)
-		for IPv6ASN in IPv6MyASNs:
+				for item in netaddr.cidr_merge(list(netaddr.iter_iprange(netblockstart,netblockend))):
+					netblocks[str(item)]=ASname
+					if options.verbose:
+						ipv4count+=1
+						if (ipv4count%20==0):
+							sys.stdout.write('.')
+							sys.stdout.flush()
+		if options.verbose:
+			ipv6count=0
+		for IPv6ASN in IPv6ASNs:
 			try:
 				ASname,netblockstartaddress,netblockendaddress,netmask=IPv6ASN
 			except:
-				print("E) An error occurred persing the IPv6ASN: "+IPv6ASN)
-			if prog.search(ASname):
-				netblock=[netaddr.IPNetwork(str(netblockstartaddress+'/'+netmask))]
-				netblocks.append(netblock)
-	print("3) Done building the list of netblocks!")
-	return [item for sublist in netblocks for item in sublist]
+				print("E) An error occurred parsing the IPv6ASN: "+IPv6ASN)
+			if prog.search(ASname,re.IGNORECASE):
+				netblock=str([netaddr.IPNetwork(str(netblockstartaddress+'/'+netmask))][0])
+				netblocks[str(netblock)]=ASname
+				if options.verbose:
+					ipv6count+=1
+					if (ipv6count%20==0):
+						sys.stdout.write(str('.'))
+						sys.stdout.flush()
+		if options.verbose:
+			print(" Found "+str(ipv4count+ipv6count)+" netblocks (IPv4:"+str(ipv4count)+"/IPv6:"+str(ipv6count)+")")
+	if options.verbose:
+		print("3) Done building the list of netblocks!")
+	return netblocks
 
-def CheckIPs(netblocks,ipfile):
+def CheckIPs(netblocks,options):
+	"""
+	Check if the given filename containing IP addresses has any that belong to the generated list of netblocks
+	"""
 	try:
-		with open(ipfile) as ipfp:
-			print("4) Checking the list of IPs ...")
+		with open(options.filename) as ipfp:
+			if options.verbose:
+				print("4) Checking the list of IPs ...")
+			else:
+				print("\"IP\",\"Network\",\"ASname\"")
 			for ip in ipfp.readlines():
 				IPInNetblock(ip.strip(),netblocks)
 	except IOError:
 		print("E) Error opening "+ipfile+"!")
 		sys.exit(1)
-	print("5) All done!")
+	if options.verbose:
+		print("5) All done!")
 
 def IPInNetblock(ip,netblocks):
-	matchingblocks=netaddr.all_matching_cidrs(ip,netblocks)
-	if len(matchingblocks)>0:
-		for matchingblock in matchingblocks:
-			print("!) "+ip+" --> "+str(matchingblock))
+	for netblock in netblocks:
+		if netaddr.IPAddress(ip) in netaddr.IPNetwork(netblock):
+			if options.verbose:
+				print("!) "+ip+" --> "+netblock+" ("+netblocks[netblock]+")")
+			else:
+				print("\""+ip+"\",\""+netblock+"\",\""+netblocks[netblock]+"\"")
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print("?) Usage: "+sys.argv[0]+" <file-with-IPs-to-check>")
-	else:
-		CheckIPs(BuildNetblocks(MyASNs),sys.argv[1])
+	cli=optparse.OptionParser(usage="usage: %prog -f <IPFILE> [options...] <list of AS names / numbers> ...\n\nE.g.: %prog -f ips.txt AS286 'KPN B.V.' BlepTech ...")
+	cli.add_option('-f','--file',dest='filename',action='store',help='[required] File with IPs to check',metavar='IPFILE')
+	cli.add_option('-q','--quiet',dest='verbose',action='store_false',default=True,help='[optional] Do not print progress, errors (quiet operation), CSV output format')
+	cli.add_option('-u','--update',dest='update',action='store_true',default=False,help='[optional] Update the GeoLite databases (obviously requires an active internet connection)')
+	(options,ASNs)=cli.parse_args()
+	if options.update:
+		if options.verbose:
+			print("Updating GeoLite IP databases from "+GeoIPURL+'...')
+		UpdateGeoIP()
+		if options.verbose:
+			print("Update done!")
+	if (not options.filename or len(ASNs)<1) and not options.update:
+		cli.print_help()
+		sys.exit(1)
+	CheckIPs(BuildNetblocks(ASNs,options),options)
